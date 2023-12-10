cbfab43ba5900373cacf43f15bd188c8d44ac20a
0xzre <alilo.ghazali@gmail.com>
2023-12-10 11:23:19 +0700

    feat: profile photo, fix: color theme, logos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { FaNapster } from "react-icons/fa";
+import { FaMugHot } from "react-icons/fa";
+import { GoRocket } from "react-icons/go";
 
 function App() {
   return (
-    <div className="bg-[#f3f3f3] text-[#333] min-h-screen px-10 sm:px-20 md:px-40">
+    <div className="bg-primary text-primary-foreground min-h-screen px-10 sm:px-20 md:px-40 pb-20">
       <nav className="flex justify-between items-center py-6">
         <div className="text-4xl font-bold text-[#bd1e59]">abd</div>
         <div className="space-x-6 text-lg">
@@ -19,29 +22,31 @@ function App() {
       </nav>
       <div className="grid grid-cols-1 gap-10">
         <div className="space-y-6">
-          <div className="flex space-x-2 justify-center">
-            Logo
+          <div className="flex space-x-6 justify-center">
+            <FaNapster className="text-[#bd1e59] text-4xl" />
+            <FaMugHot className="text-[#bd1e59] text-4xl" />
+            <GoRocket className="text-[#bd1e59] text-4xl" />
           </div>
           <h1 className="text-5xl font-semibold text-center">
-            Programmer
+          Hobbyist Programmer, Fulltime Cavemen.
           </h1>
           <p className="text-lg text-center">Your usual CS student.</p>
-          <div className="flex justify-center">
+          {/* <div className="flex justify-center">
             <Button className="bg-[#bd1e59] text-white py-2 px-4">
               Discover my work
             </Button>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
+          </div> */}
+          {/* <div className="flex items-center justify-center space-x-2">
             alilo.ghazali
             <span>@gmail.com</span>
-          </div>
+          </div> */}
         </div>
         <div className="space-y-6">
           <img
-            alt="Illustration of a person"
+            alt="abd photo"
             className="rounded-lg mx-auto"
             height="400"
-            src="/placeholder.svg"
+            src="./src/assets/photo.jpg"
             style={{
               aspectRatio: "600/400",
               objectFit: "cover",
